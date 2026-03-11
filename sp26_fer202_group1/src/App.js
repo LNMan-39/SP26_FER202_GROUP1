8ec1e57868db0cd7e1e17314884fb95c401e79f0
Tramknce191474 <tramkn.ce191474@gmail.com>
2026-03-12 02:03:01 +0700

    Thêm Register và AuthContext

--- sp26_fer202_group1/src/App.js
+++ sp26_fer202_group1/src/App.js
@@ -1,26 +1,27 @@
-import AuthContext from "./context/AuthContext";
+import AuthProvider from "./context/AuthContext";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Contact from "./pages/Contact";
 import AdminContact from "./pages/AdminContact";
+import Register from "./pages/Register";
 
 function App() {
 
   return (
 
-    <AuthContext>
+    <AuthProvider>
       <Router>
         <Routes>
           <Route path="/contact" element={<Contact />} />
-
+          <Route path="/register" element={<Register />} />
           <Route path="/admin/contacts" element={<AdminContact />} />
 
 
 
         </Routes>
       </Router >
-    </AuthContext >
+    </AuthProvider >
   );
 }
 
