f579c0b08ef940a869b257be11c83f44e2d27cd0
unknown <hphuocoa@gmail.com>
2026-03-11 13:02:28 +0700

    add Contact and AdminContact pages

--- sp26_fer202_group1/src/App.js
+++ sp26_fer202_group1/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import AuthContext from "./context/AuthContext";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Contact from "./pages/Contact";
+import AdminContact from "./pages/AdminContact";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <AuthContext>
+      <Router>
+        <Routes>
+          <Route path="/contact" element={<Contact />} />
+
+          <Route path="/admin/contacts" element={<AdminContact />} />
+
+
+
+        </Routes>
+      </Router >
+    </AuthContext >
   );
 }
 
---
+++ sp26_fer202_group1/src/pages/Contact.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import axios from "axios";
+
+function Contact() {
+
+    const [validated, setValidated] = useState(false);
+    const [formData, setFormData] = useState({ name: "", email: "", message: "" });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+
+            event.stopPropagation();
+        } else {
+
+            await axios.post("http://localhost:3002/contacts", formData);
+            alert("Messege sent successfully!");
+            setFormData({ name: "", email: "", message: "" });
+        }
+        setValidated(true);
+    };
+    const handleReset = () => {
+        setFormData({ name: "", email: "", message: "" });
+        setValidated(false);
+    };
+
+    return (
+        <div>
+            <h2>Contact Us</h2>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control required type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Enter your name" />
+
+                </Form.Group>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control required type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Enter your email" />
+
+                </Form.Group>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control required as="textarea" rows={3} name="message" value={formData.message} onChange={handleChange} placeholder="Enter your message" />
+                </Form.Group>
+                <Button type="submit" className="m-2">Submit</Button>
+
+                <Button type="button" variant="secondary" onClick={handleReset}> Reset </Button>
+
+            </Form>
+
+        </div>
+    );
+}
+export default Contact;
